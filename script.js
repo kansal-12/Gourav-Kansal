c856a8091332f5309b7062956e43cdaa36694c81
kansal-12 <gouravkansal595@gmail.com>
2025-11-15 14:24:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Smooth Scroll for Anchor Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Scroll to Top Button
+window.onscroll = function() {
+    let topBtn = document.getElementById("scrollToTop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
